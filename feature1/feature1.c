91d90bcd71e2c7a575d8f55569a8272d86bcde6a
Ayush Gupta <captainayush19@gmail.com>
2026-03-09 20:36:39 +0530

    Implement audio volume modification feature

--- feature1/feature1.c
+++ feature1/feature1.c
@@ -1,11 +1,57 @@
-#include<stdio.h>
+// Modifies the volume of an audio file
 
-void feature1(){
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    float factor = atof(argv[3]);
+
+    // TODO: Copy header from input file to output file
+
+    uint8_t header[HEADER_SIZE]; // header: buffer as array of bytes
+
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
+
+    // TODO: Read samples from input file and write updated data to output file
+
+    int16_t buffer; // 2 byte
+
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer = buffer * factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
 }
 
-int main(){
-
-  feature1();
-  return 0;
-}
